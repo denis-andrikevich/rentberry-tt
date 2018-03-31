32638c0f2ca6a98024bc0b33fe7747a4383cf22b
Denis <denis@backedinc.com>
2018-03-31 16:31:01 +0300

    Add material2

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
-// libs
 import { TransferHttpCacheModule } from '@nguniversal/common';
 
-// components
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
-// import { ServiceWorkerModule } from '@angular/service-worker';
-// the Request object only lives on the server
-export function getRequest(): any {
-  return { headers: { cookie: document.cookie } };
-}
 
 @NgModule({
   bootstrap: [AppComponent],
--- src/main.browser.ts
+++ src/main.browser.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppBrowserModule } from './app/app.browser.module';
 import { environment } from './environments/environment';
 
+import 'hammerjs';
+
 if (environment.production) {
   enableProdMode();
 }
